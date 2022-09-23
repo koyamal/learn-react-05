a3c1858826fdc12f69486260d631cb0e1ed0013b
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-23 10:57:11 +0900

    Add style into UserCard

--- src/components/organisms/user/UserCard.jsx
+++ src/components/organisms/user/UserCard.jsx
@@ -1,18 +1,36 @@
+import styled from "styled-components";
+
 export const UserCard = () => {
   return (
     <div>
-      <img alt="profile" />
+      <img
+        width={160}
+        src="https://source.unsplash.com/5PVXkqt2s9k"
+        alt="profile"
+      />
       <p>Name</p>
-      <dl>
+      <SDl>
         <dt>E-mail</dt>
-        <dd>testemailadress@aaaa.com</dd>
+        <dd>test@aaaa.com</dd>
         <dt>Tell</dt>
         <dd>111-2222-3344</dd>
-        <dt>Company Name</dt>
+        <dt>Company</dt>
         <dd>XXX YYY Co., Ltd.</dd>
         <dt>WEB</dt>
         <dd>abcdefgaaa.com</dd>
-      </dl>
+      </SDl>
     </div>
   );
 };
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 55px;
+    padding-bottom: 8px;
+  }
+`;

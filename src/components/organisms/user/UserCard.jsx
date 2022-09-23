dfda908266685d871756764016b3bff9cda143fa
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-23 10:46:23 +0900

    Create User Card

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="profile" />
+      <p>Name</p>
+      <dl>
+        <dt>E-mail</dt>
+        <dd>testemailadress@aaaa.com</dd>
+        <dt>Tell</dt>
+        <dd>111-2222-3344</dd>
+        <dt>Company Name</dt>
+        <dd>XXX YYY Co., Ltd.</dd>
+        <dt>WEB</dt>
+        <dd>abcdefgaaa.com</dd>
+      </dl>
+    </div>
+  );
+};

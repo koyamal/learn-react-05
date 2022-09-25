54587621cad29544ca03efad8ac019d7961079cf
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-25 14:46:14 +0900

    Add users card to users page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  image: "https://source.unsplash.com/5PVXkqt2s9k",
-  name: "Tom",
-  email: "test@aaaa.com",
-  phone: "111-2222-3344",
-  company: {
-    name: "XXX YYY Co., Ltd."
-  },
-  website: "abcdefgaaa.com"
-};
-
 export default function App() {
   return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,36 @@
 import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    image: "https://source.unsplash.com/5PVXkqt2s9k",
+    name: `Tom${val}`,
+    email: "test@aaaa.com",
+    phone: "111-2222-3344",
+    company: {
+      name: "XXX YYY Co., Ltd."
+    },
+    website: "abcdefgaaa.com"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>Users List!</h2>
+      <SearchInput />
+      {users.map((user) => {
+        return <UserCard key={user.id} user={user} />;
+      })}
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
 `;

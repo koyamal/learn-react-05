b90c8a0d66a87f0227ab352d0fecd9922f7cea59
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-25 14:27:35 +0900

    Make Top page and Users page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 import "./styles.css";
 
 const user = {
@@ -19,15 +13,5 @@ const user = {
 };
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>Test</PrimaryButton>
-        <SecondaryButton>Go!</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>Users List!</h2>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+`;

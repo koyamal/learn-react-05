f1941709a811da155d1a4761a752cf17840719a9
koyamal <74910401+koyamal@users.noreply.github.com>
2022-09-25 14:11:38 +0900

    Create Footer and Default layout

--- src/components/atoms/layout/Footer.jsx
+++ src/components/atoms/layout/Footer.jsx
@@ -2,21 +2,15 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Footer = () => {
-  return (
-    <SHeader>
-      <SLink to="/">HOME</SLink>
-      <SLink to="/users">USERS</SLink>
-    </SHeader>
-  );
+  return <SFooter>&copy; 2022 Tom Inc.</SFooter>;
 };
 
-const SHeader = styled.header`
+const SFooter = styled.footer`
   background-color: #11999e;
   color: #fff;
   text-align: center;
   padding: 8px 0;
-`;
-
-const SLink = styled(Link)`
-  margin: 0 8px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
 `;
